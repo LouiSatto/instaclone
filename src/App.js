a2c080d3990e977db6280f8eb570e300f460d616
Loui Satto <x_sato@hotmail.com>
2020-06-06 17:40:47 +0900

    instalando dependencias

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Teste instaclone app dentro de src</Text>
+      <Text>Teste instaclone reinstalado modules e dependencias</Text>
     </View>
   );
 }
