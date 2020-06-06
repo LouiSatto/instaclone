a15911f69d752f8f0ae7814c63a91f5440342592
Loui Satto <x_sato@hotmail.com>
2020-06-06 17:47:10 +0900

    dependences installed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Teste instaclone reinstalado modules e dependencias</Text>
+      <Text>Teste instaclone dependencia redux</Text>
     </View>
   );
 }
