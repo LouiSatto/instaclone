300e56733a6386f850b653f62876918c181adede
Loui Satto <x_sato@hotmail.com>
2020-06-08 22:22:38 +0900

    Comments ok

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import Header from './components/Header'
 import Post from './components/Post'
 
 export default function App() {
+
+  const comments = [{
+    nickname: 'Maria do Carmo',
+    comment: 'Mais ou menos isso hein!'
+  },{
+    nickname: 'Jaburanga',
+    comment: 'Q Bosta, ta zuado isso ai hein'
+  }]
   return (
     <View style={{flex: 1, backgroundColor: '#000'}}>
       <Header/>
-      <Post image={require('../assets/imgs/fence.jpg')} />
+      <Post image={require('../assets/imgs/fence.jpg')} comments={comments}/>
     </View>
   );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react'
 import {View, Text, Image, StyleSheet, Dimensions} from 'react-native'
 
 import Author from './Author'
+import Comments from './Comments'
 
 class Post extends Component {
     render() {
         return (
             <View style = {styles.container}>
                 <Image style = {styles.image} source = {this.props.image}></Image>
-                <Author email = 'sulano@gmail.com' nickName = 'Fulano de Tal' />
+                <Author email = 'fulano@gmail.com' nickName = 'Fulano de Tal' />
+                <Comments comments = {this.props.comments}></Comments>
             </View>
         )
     }
